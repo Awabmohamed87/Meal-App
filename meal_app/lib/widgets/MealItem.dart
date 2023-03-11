4a233d7a66f69b752c1035874972f32274993af8
Awabmohamed87 <62722055+Awabmohamed87@users.noreply.github.com>
2023-03-11 16:45:24 +0200

    lesson 8

--- meal_app/lib/widgets/MealItem.dart
+++ meal_app/lib/widgets/MealItem.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
   final String imageUrl;
+  final String title;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
 
-  const MealItem(this.imageUrl);
+  const MealItem(
+      {@required this.imageUrl,
+      @required this.title,
+      @required this.duration,
+      @required this.complexity,
+      @required this.affordability});
   void selectMeal() {}
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return "Simple";
+        break;
+      case Complexity.Hard:
+        return "Hard";
+        break;
+      case Complexity.Challenging:
+        return "Challenging";
+        break;
+      default:
+        return "Unknown";
+        break;
+    }
+  }
+
+  String get affotrdabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return "Affordable";
+        break;
+      case Affordability.Luxurious:
+        return "Luxurious";
+        break;
+      case Affordability.Pricey:
+        return "Pricey";
+        break;
+      default:
+        return "Unknown";
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -23,8 +67,48 @@ class MealItem extends StatelessWidget {
                     topRight: Radius.circular(15),
                   ),
                   child: Image.network(imageUrl),
-                )
+                ),
+                Positioned(
+                  right: 10,
+                  bottom: 5,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    width: 300,
+                    color: Colors.black54,
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(width: 5),
+                      Text("$duration min"),
+                    ],
+                  ),
+                  Row(children: [
+                    Icon(Icons.work),
+                    SizedBox(width: 5),
+                    Text("$complexityText"),
+                  ]),
+                  Row(children: [
+                    Icon(Icons.attach_money),
+                    SizedBox(width: 5),
+                    Text("$affotrdabilityText"),
+                  ])
+                ],
+              ),
             )
           ],
         ),
